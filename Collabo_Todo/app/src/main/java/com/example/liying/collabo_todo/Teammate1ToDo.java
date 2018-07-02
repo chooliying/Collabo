7b4d26fb89f6c8f684a6f55e88e590c1645e4195
Liying <cliying.lc@gmail.com>
2018-07-02 13:42:48 +0800

    first commit

---
+++ Collabo_Todo/app/src/main/java/com/example/liying/collabo_todo/Teammate1ToDo.java
@@ -0,0 +1,13 @@
+package com.example.liying.collabo_todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Teammate1ToDo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teammate1_to_do);
+    }
+}
